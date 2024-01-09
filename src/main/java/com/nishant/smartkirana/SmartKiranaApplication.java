ba8620c1920e5be96cec52c74e373a57b2a87244
Nishant - personal <nishant-personal@Nishant-mac.local>
2024-01-09 22:01:56 +0530

    Initial commit

---
+++ src/main/java/com/nishant/smartkirana/SmartKiranaApplication.java
@@ -0,0 +1,13 @@
+package com.nishant.smartkirana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKiranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKiranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishant/smartkirana/SmartKiranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.smartkirana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartKiranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
